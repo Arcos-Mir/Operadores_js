54d7b3d701db385de25e96977a0b67429091f929
Mirza Arcos <166764651+Arcos-Mir@users.noreply.github.com>
2024-05-29 01:45:07 -0600

    Add files via upload

---
+++ oper.js
@@ -0,0 +1,101 @@
+/* Tipos de operadores:
+        - Asignacion
+        - Matematicos o aritmeticos
+        - Incremento y Decremento
+        - Operadores logicos o de comparacion
+            - Negacion !
+            - AND      &&
+            - OR       ||
+    */
+
+//NOS PERMITEN MANIPULAR EL VALOR DE LAS VARIABLES SE PUEDEN HACER OPERACVIONES Y COMPARACIONES
+
+var nombre;
+nombre = 'Mir'
+
+// ----- Matematicos o Aritmeticos
+
+/*var num1 = 10;
+var num2 = 5;
+var res = num1 + num2;
+var res = num1 - num2;
+var res = num1 * num2;
+var res = num1 / num2;
+var res = num1 % num2;//% nos devuelve el resultado de una division
+document.write(res);*/
+
+// ----- incremento y Decremento
+
+/*var num = 5;
+//var res = num -1;//una opcion para hacer la operacion de decremento
+//var res = --num;//otra opcion en la que una unidad se representa con "--"
+//se le esat diciendo a la operacion haz un decremento de una unidad a la variable num
+var res = ++num;//++ incrementa una unidad a la variable num
+document.write(res);*/
+
+// ----- Operadores logicos o de comparacion
+
+        /*Operadores :
+                == Igual a
+                === Igual en valor y tipo
+                !=  No iguales
+                !== No iguales en valor y tipo
+                >   Mayor que
+                <   Menor que
+                >=  Mayor o igual que
+                <=  menor o igual que*/
+
+/*var num1 = 5;
+//var num2 = 5;  
+var res = num1 == num2;
+document.write(res);   //true*/
+
+/*var num1 = 5;
+var num2 = 15;  
+var res = num1 == num2;
+document.write(res);  //false*/
+
+/*var num1 = '5';
+var num2 = 5;  
+var res = num1 == num2;
+document.write(res);   //true*/
+
+/*var num1 = '5';
+var num2 = 5;  
+var res = num1 === num2;
+document.write(res);  //false*/
+
+/*var num1 = 5;
+var num2 = 10;  
+var res = num1 != num2;
+document.write(res);  //true*/
+
+/*var num1 = 5;
+var num2 = 5;  
+var res = num1 != num2;
+document.write(res);  //false*/
+
+/*var num1 = 5;
+var num2 = '5';  
+var res = num1 !== num2;
+document.write(res); //true}*/
+
+/*var num1 = 5;
+var num2 = '10';  
+var res = num1 !== num2; 
+document.write(res);  //true*/
+
+/*var num1 = 5;
+var num2 = 10;  
+var res = num1 > num2; 
+document.write(res);  //false*/
+
+/*var num1 = 5;
+var num2 = 10;  
+var res = num1 < num2; 
+document.write(res);   //true*/
+
+var num1 = 10;
+var num2 = 10;  
+var res = num1 <= num2; 
+document.write(res);  //true porque no es menor pero es igual a num2
